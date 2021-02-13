012160dc16acbc600294bfefa62eaeaac6a139a9
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2021-02-13 12:08:03 +0400

    Add exception renderer concept to render the exceptions in a proper format

---
+++ app/Core/Exception/Renderer/minimalExceptionView.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The exception view
+ *
+ * @category Exception
+ * @package  Test project
+ * @author   Hamed Ghasempour <hamedghasempour@gmail.com>
+ * @link     null
+ */
+
+
+/**
+ * @var Throwable $throwable
+ */
+?>
+
+<html>
+<head>
+    <title>Error!</title>
+</head>
+<body style="margin: 0; font-family: calibri,serif; background-color: #dbdbdb">
+<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100%;">
+    <div style="padding: 20px; background-color: #f27a00; color:#fff; font-weight: bold; font-size: 25px; border-bottom : 5px solid #333;">
+        Something Wrong happened!
+    </div>
+</div>
+</body>
+</html>
