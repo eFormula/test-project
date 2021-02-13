e66c75d5bd6f3ce8c29a240a813a23c0e1957818
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2021-02-13 12:04:02 +0400

    Add an exception to use in not found situations

---
+++ app/Exceptions/NotFoundException.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Throwable;
+
+/**
+ * The not found exception
+ * PHP version >= 7.0
+ *
+ * @category Exceptions
+ * @package  Test project
+ * @author   Hamed Ghasempour <hamedghasempour@gmail.com>
+ */
+class NotFoundException extends Exception
+{
+    /**
+     * CasinoException constructor.
+     *
+     * @param string         $message  The message of Exception
+     * @param int            $code     The code of Exception
+     * @param Throwable|null $previous The previous throwable used for the exception chaining.
+     */
+    public function __construct(
+        $message = "Not found.",
+        $code = 404,
+        Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+}
